08ea1b64457cc68c498abaeb7b6aecac5c7c0400
vrajpatel-18 <103467937+vrajpatel-18@users.noreply.github.com>
2022-04-11 19:38:38 -0500

    Update poeltl.js

--- poeltl.js
+++ poeltl.js
@@ -1,3 +1,8 @@
+//Adjust screen width for mobile
+let siteWidth = 1280;
+let scale = screen.width /siteWidth;
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
 const playerList = [
     "Precious Achiuwa",
     "Steven Adams",
